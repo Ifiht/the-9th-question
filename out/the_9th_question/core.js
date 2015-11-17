c8641eced8bb2d5e9f4cf3e485c943609d80222b
Charles <postmaster@emersea.com>
2015-11-17 14:38:48 -0600

    Finalized build scripts

--- out/the_9th_question/core.js
+++ out/the_9th_question/core.js
@@ -1,7 +1,5 @@
-// Compiled by ClojureScript 1.7.170 {}
+// Compiled by ClojureScript 1.7.170 {:static-fns true, :optimize-constants true}
 goog.provide('the_9th_question.core');
 goog.require('cljs.core');
-cljs.core.enable_console_print_BANG_.call(null);
-cljs.core.println.call(null,"Hello world!");
-
-//# sourceMappingURL=core.js.map
+cljs.core.enable_console_print_BANG_();
+cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.array_seq(["Hello world!"], 0));
--- src/the_9th_question/core.cljs
+++ src/the_9th_question/core.cljs
@@ -1,4 +1,5 @@
 (ns the-9th-question.core)
 
 (enable-console-print!)
+
 (println "Hello world!")
